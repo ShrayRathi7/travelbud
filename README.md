73e47f0ced3dcaa18bf084d5090122207e6e732f
ShrayRathi7 <125061576+ShrayRathi7@users.noreply.github.com>
2024-04-28 12:29:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# travelbud
+# TravelBuddy:airplane:
+
+:globe_with_meridians:[TravelBuddy](https://meran0.github.io/TravelBuddy/) website system are revolving around
+tourism conceptualization that focuses on the
+topic of the tourist guide system as the focal point.
+
+Our website is focusing on booking system
+for the tourist guide based on the packages
+and location provided by the customer.
+
+Our website also provides the most viewed
+location and also previous reviews from our past user so
+they have trouble-free moments to infer their decision
+
+Group Members:
+* MUHAMMAD IKRAM BIN IQBAL FITHRI   
+  * 1912071 | 
+* MMUHAMMAD IMRAN BIN MOHAMAD
+  * 1912837 | 
+* NURULAIMI HANNANI BINTI MOHAMMAD
+  * 1912728 | 
+* ANI AFIQAH BINTI ZAMRUD
+  * 1912406 | 
+* NUR QHAISARA BINTI MOHD ZAKZAM
+  * 1913074 | 
+
+## Resources
+
+Video : Malaysia timelapse / hyperlapse by Duntakk
+
+Link : [Youtube](https://youtu.be/Le7QAafIRj8?t=29)
+
+Booking Data Set : malaysian-states by Muhammad Hazeem
+
+Link : [GitHub](https://github.com/hazz1925/malaysian-states)
---
+++ homepage.js
@@ -0,0 +1,51 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("sideBtn").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("sideBtn").style.marginLeft= "0";
+}
+
+function myFunction() {
+  location.href = "https://www.makemytrip.com/";
+}
+
+var mybutton = document.getElementById("TopMenu");
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function genReview() {
+  var randNum = Math.floor(Math.random() * 4) + 1;
+  document.getElementById('review').innerHTML = quotes[randNum];
+}
+
+//review array
+var quotes = ["Blank",
+"\"The hotel was simple amazing and I couldn't thank Travel Buddy more for helping out. I've been a customer for awhile and I have to say it's probably my favourite web!\"<br>-  Rahim from Terengganu",
+"\"Looks exactly what is posted here in Travel Buddy. Great service!\"<br> - John Malik from Kedah",
+"\"I could not have asked for a better stay than Lexis Hibiscus. Thanks to Travel Buddy.\"<br>- Mark Lee from Sarawak",
+"\"Had an excellent recommendations!\"<br>- Mutusami from KL",];
